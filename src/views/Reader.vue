421f8d40568f82411ef97654caece62e359f4cc7
SerdarSerifoglu <serdarserifoglu@gmail.com>
2020-08-28 22:58:10 +0300

    hover popup işlemi eklendi.

--- src/views/Reader.vue
+++ src/views/Reader.vue
@@ -1,7 +1,46 @@
 <style>
-.word {
+.tooltip {
+  border-bottom: 1px dotted #000000;
+  color: #000000;
+  outline: none;
+  cursor: help;
+  text-decoration: none;
+  position: relative;
   color: red;
   font-weight: bolder;
+}
+.tooltip span {
+  margin-left: -999em;
+  position: absolute;
+}
+.tooltip:hover span {
+  border-radius: 5px 5px;
+  -moz-border-radius: 5px;
+  -webkit-border-radius: 5px;
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
+  -webkit-box-shadow: 5px 5px rgba(0, 0, 0, 0.1);
+  -moz-box-shadow: 5px 5px rgba(0, 0, 0, 0.1);
+  font-family: Calibri, Tahoma, Geneva, sans-serif;
+  position: absolute;
+  left: 1em;
+  top: 2em;
+  z-index: 99;
+  margin-left: 0;
+  width: 250px;
+}
+.tooltip:hover em {
+  font-family: Candara, Tahoma, Geneva, sans-serif;
+  font-size: 1.2em;
+  font-weight: bold;
+  display: block;
+  padding: 0.2em 0 0.6em 0;
+}
+.custom {
+  padding: 0.5em 0.8em 0.8em 2em;
+}
+.critical {
+  background: #ffccaa;
+  border: 1px solid #ffad33;
 }
 </style>
 
@@ -41,12 +80,16 @@ export default {
           // console.log(this.article.toUpperCase().includes("APPLE"));
           this.article = this.article.toLowerCase();
           response.data.data.forEach(element => {
-            if (this.article.includes(element.valEng1.toLowerCase())) {
+            if (
+              this.article.includes(" " + element.valEng1.toLowerCase() + " ")
+            ) {
               this.article = this.article.replace(
                 element.valEng1.toLowerCase(),
-                `<span class='word' title='${
-                  element.valTr1
-                }'>${element.valEng1.toLowerCase()}</span>`
+                `<span class='tooltip'>${element.valEng1.toLowerCase()}
+                 <span class="custom critical">
+                <em>${element.valEng1.toLowerCase()}</em>
+                ${element.valTr1.toLowerCase()}</span>
+              </span>`
               );
             }
           });
