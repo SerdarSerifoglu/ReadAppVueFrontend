37f9a28ca378db28fe527e1e63ccc3de142b4ccb
SerdarSerifoglu <serdarserifoglu@gmail.com>
2020-10-04 10:26:44 +0300

    Temizlik(gereksiz kodlar kaldırılıyor)

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 import Register from "../views/Register.vue";
 import Reader from "../views/Reader.vue";
 import PackAdd from "../views/pack/Add.vue";
@@ -11,51 +10,37 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  },
-  {
     path: "/login",
     name: "Login",
-    component: Login
+    component: Login,
   },
   {
     path: "/register",
     name: "Register",
-    component: Register
+    component: Register,
   },
   {
     path: "/reader",
     name: "Reader",
-    component: Reader
+    component: Reader,
   },
   {
     path: "/pack",
     name: "PackAdd",
     component: PackAdd,
-    children: [{ path: "", component: PackAdd }]
+    children: [{ path: "", component: PackAdd }],
   },
   {
     path: "/test",
     name: "Test",
-    component: Test
-  }
+    component: Test,
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
