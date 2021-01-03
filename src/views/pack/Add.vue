80f559f1672d5e08c76b875c5443de12306bcf7a
SerdarSerifoglu <serdarserifoglu@gmail.com>
2021-01-03 18:53:37 +0300

    my pack ekranında modal elden geçirildi. Reader ekranındaki modal'da burdaki gibi yapılacak (belki modal için bir component yapılabilir.)

--- src/views/pack/Add.vue
+++ src/views/pack/Add.vue
@@ -1,27 +1,22 @@
 <style scoped>
-.btnSuccess {
-  color: #fff;
-  background-color: #28a745;
-  border-color: #28a745;
-}
-.btnDanger {
-  color: #fff;
-  background-color: #dc3545;
-  border-color: #dc3545;
-}
-th,
-td {
-  text-align: center;
-}
+  .btnSuccess {
+    color: #fff;
+    background-color: #28a745;
+    border-color: #28a745;
+  }
+  .btnDanger {
+    color: #fff;
+    background-color: #dc3545;
+    border-color: #dc3545;
+  }
+  th,
+  td {
+    text-align: center;
+  }
 </style>
 <template>
   <div class="packAdd">
-    <button
-      class="btn btn-primary col-md-3 col-xs-12"
-      @click="addNewPackButtonClick"
-      data-toggle="modal"
-      data-target=".bd-example-modal-sm"
-    >
+    <button type="button" class="btn btn-primary" @click="addNewPackButtonClick()">
       Add New Pack
     </button>
     <div class="table-responsive">
@@ -45,11 +40,7 @@ td {
                   btnSuccess: !pack.isShared,
                   btnDanger: pack.isShared,
                 }"
-                v-text="
-                  pack.isShared
-                    ? sharedButtonTextShareCancel
-                    : sharedButtonTextShare
-                "
+                v-text="pack.isShared ? sharedButtonTextShareCancel : sharedButtonTextShare"
                 @click="shareButtonClick(pack._id)"
               ></button>
             </td>
@@ -63,10 +54,7 @@ td {
                 Edit
               </button>
               <span> | </span>
-              <button
-                class="btn btn-sm btn-danger"
-                @click="deleteButtonClick(pack._id)"
-              >
+              <button class="btn btn-sm btn-danger" @click="deleteButtonClick(pack._id)">
                 Delete
               </button>
             </td>
@@ -83,47 +71,64 @@ td {
       role="dialog"
       aria-labelledby="mySmallModalLabel"
       aria-hidden="true"
-      style="margin: 5rem"
+      v-bind:class="{
+        show: modalDisplay,
+      }"
+      v-bind:style="{
+        display: modalDisplayValue,
+      }"
     >
-      <div class="modal-dialog">
+      <div class="modal-dialog" id="addPackModal">
         <div class="modal-content">
-          <div style="margin: 5rem">
-            <app-input
-              @input="$v.packData.title.$touch()"
-              :inputClass="{
-                'form-control': true,
-                'is-invalid': $v.packData.title.$error,
-              }"
-              inputId="title"
-              label="Title"
-              divClass="col-md-12 col-xs-12"
-              v-model="packData.title"
-              :dataValue="packData.title"
-              :attention1="$v.packData.title.required"
-              attention1Text="Title is required !"
-            ></app-input>
-            <app-input
-              @input="$v.packData.description.$touch()"
-              :inputClass="{
-                'form-control': true,
-                'is-invalid': $v.packData.description.$error,
-              }"
-              inputId="description"
-              label="Description"
-              divClass="col-md-12 col-xs-12"
-              v-model="packData.description"
-              :dataValue="packData.description"
-              :attention1="$v.packData.description.required"
-              attention1Text="Description is required !"
-            ></app-input>
-
-            <button
-              class="btn btn-success col-md-3 center"
-              @click="insertOrUpdateWord()"
-              :disabled="$v.packData.$invalid"
-            >
-              ADD
-            </button>
+          <div style="margin: 2rem">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLongTitle">Save Pack</h5>
+              <button
+                type="button"
+                @click="modalClick()"
+                style="border: 0;padding: 3px;background-color:#fff; cursor: pointer;"
+              >
+                <span>X</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <app-input
+                @input="$v.packData.title.$touch()"
+                :inputClass="{
+                  'form-control': true,
+                  'is-invalid': $v.packData.title.$error,
+                }"
+                inputId="title"
+                label="Title"
+                divClass="col-md-12 col-xs-12"
+                v-model="packData.title"
+                :dataValue="packData.title"
+                :attention1="$v.packData.title.required"
+                attention1Text="Title is required !"
+              ></app-input>
+              <app-input
+                @input="$v.packData.description.$touch()"
+                :inputClass="{
+                  'form-control': true,
+                  'is-invalid': $v.packData.description.$error,
+                }"
+                inputId="description"
+                label="Description"
+                divClass="col-md-12 col-xs-12"
+                v-model="packData.description"
+                :dataValue="packData.description"
+                :attention1="$v.packData.description.required"
+                attention1Text="Description is required !"
+              ></app-input>
+            </div>
+            <div class="modal-footer">
+              <button class="btn btn-success center" @click="insertOrUpdateWord()" :disabled="$v.packData.$invalid">
+                <i class="far fa-save" style="margin-right:3px;"></i> SAVE
+              </button>
+              <button class="btn btn-danger center" @click="modalClick()">
+                <i class="far fa-window-close" style="margin-right:3px;"></i> Close
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -133,56 +138,89 @@ td {
 </template>
 
 <script>
-import axios from "axios";
-import { required } from "vuelidate/lib/validators";
-import Input from "../../components/Input";
+  import axios from "axios";
+  import { required } from "vuelidate/lib/validators";
+  import Input from "../../components/Input";
 
-export default {
-  data() {
-    return {
-      sharedButtonTextShare: "Share",
-      sharedButtonTextShareCancel: "Share Cancel",
-      packData: {
-        title: "",
-        description: "",
-      },
-      packs: [],
-    };
-  },
-  validations: {
-    packData: {
-      title: { required },
-      description: { required },
+  export default {
+    data() {
+      return {
+        modalDisplayValue: "none",
+        modalDisplay: false,
+        sharedButtonTextShare: "Share",
+        sharedButtonTextShareCancel: "Share Cancel",
+        packData: {
+          title: "",
+          description: "",
+        },
+        packs: [],
+      };
     },
-  },
-  name: "PackAdd",
-  components: {
-    "app-input": Input,
-  },
-  methods: {
-    insertOrUpdateWord: async function () {
-      //işlem uygulanıcak data varsa update yoksa add
-      if (this.packData._id === undefined) {
-        await axios
-          .post(
-            "http://localhost:5000/api/pack/add/",
-            { ...this.packData },
-            {
-              headers: {
-                Authorization: `Bearer: ${this.$store.state.token}`,
-              },
-            }
-          )
-          .then((response) => {
-            console.log(response);
-            alert("Pack added");
-          })
-          .catch((e) => console.log(e));
-      } else {
+    validations: {
+      packData: {
+        title: { required },
+        description: { required },
+      },
+    },
+    name: "PackAdd",
+    components: {
+      "app-input": Input,
+    },
+    methods: {
+      modalClick: function() {
+        this.modalDisplay = !this.modalDisplay;
+        if (this.modalDisplay) {
+          this.modalDisplayValue = "block";
+        } else {
+          this.modalDisplayValue = "none";
+        }
+      },
+      insertOrUpdateWord: async function() {
+        //işlem uygulanıcak data varsa update yoksa add
+        if (this.packData._id === undefined) {
+          await axios
+            .post(
+              "http://localhost:5000/api/pack/add/",
+              { ...this.packData },
+              {
+                headers: {
+                  Authorization: `Bearer: ${this.$store.state.token}`,
+                },
+              }
+            )
+            .then((response) => {
+              console.log(response);
+              alert("Pack added");
+            })
+            .catch((e) => console.log(e));
+        } else {
+          await axios
+            .put(
+              "http://localhost:5000/api/pack/" + this.packData._id,
+              { ...this.packData },
+              {
+                headers: {
+                  Authorization: `Bearer: ${this.$store.state.token}`,
+                },
+              }
+            )
+            .then((response) => {
+              console.log(response);
+              alert("Pack updated");
+            })
+            .catch((e) => console.log(e));
+        }
+        this.packData = {};
+      },
+      async addNewPackButtonClick() {
+        this.packData = {};
+        this.modalClick();
+      },
+      async shareButtonClick(packId) {
         await axios
           .put(
-            "http://localhost:5000/api/pack/" + this.packData._id,
-            { ...this.packData },
+            "http://localhost:5000/api/pack/" + packId + "/shared",
+            {},
             {
               headers: {
                 Authorization: `Bearer: ${this.$store.state.token}`,
@@ -190,67 +228,46 @@ export default {
             }
           )
           .then((response) => {
+            this.refreshList();
             console.log(response);
-            alert("Pack updated");
+            // this.packComboboxChange(this.selectedPack);
           })
           .catch((e) => console.log(e));
-      }
-      this.packData = {};
-    },
-    async addNewPackButtonClick() {
-      this.packData = {};
-    },
-    async shareButtonClick(packId) {
-      await axios
-        .put(
-          "http://localhost:5000/api/pack/" + packId + "/shared",
-          {},
-          {
+      },
+      editButtonClick: async function(packId, packTitle, packDescription) {
+        this.packData._id = packId;
+        this.packData.title = packTitle;
+        this.packData.description = packDescription;
+        this.modalClick();
+      },
+      deleteButtonClick: async function(packId) {
+        await axios
+          .delete("http://localhost:5000/api/pack/" + packId, {
             headers: {
               Authorization: `Bearer: ${this.$store.state.token}`,
             },
-          }
-        )
-        .then((response) => {
-          this.refreshList();
-          console.log(response);
-          // this.packComboboxChange(this.selectedPack);
-        })
-        .catch((e) => console.log(e));
+          })
+          .then((response) => {
+            response;
+            this.refreshList();
+          })
+          .catch((e) => console.log(e));
+      },
+      async refreshList() {
+        await axios
+          .get("http://localhost:5000/api/pack/getAllUsersPacks", {
+            headers: {
+              Authorization: `Bearer: ${this.$store.state.token}`,
+            },
+          })
+          .then((response) => {
+            this.packs = response.data.data;
+          })
+          .catch((e) => console.log(e));
+      },
     },
-    editButtonClick: async function (packId, packTitle, packDescription) {
-      this.packData._id = packId;
-      this.packData.title = packTitle;
-      this.packData.description = packDescription;
+    async created() {
+      await this.refreshList();
     },
-    deleteButtonClick: async function (packId) {
-      await axios
-        .delete("http://localhost:5000/api/pack/" + packId, {
-          headers: {
-            Authorization: `Bearer: ${this.$store.state.token}`,
-          },
-        })
-        .then((response) => {
-          response;
-          this.refreshList();
-        })
-        .catch((e) => console.log(e));
-    },
-    async refreshList() {
-      await axios
-        .get("http://localhost:5000/api/pack/getAllUsersPacks", {
-          headers: {
-            Authorization: `Bearer: ${this.$store.state.token}`,
-          },
-        })
-        .then((response) => {
-          this.packs = response.data.data;
-        })
-        .catch((e) => console.log(e));
-    },
-  },
-  async created() {
-    await this.refreshList();
-  },
-};
+  };
 </script>
