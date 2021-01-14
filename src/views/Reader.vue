5644797157ccb55abf5ea1d43b19ebab293986b3
SerdarSerifoglu <serdarserifoglu@gmail.com>
2021-01-14 22:52:01 +0300

    Reader ekranındaki modal, My Packs ekranındaki modal şeklinde düzenlendi.

--- src/views/Reader.vue
+++ src/views/Reader.vue
@@ -1,49 +1,49 @@
 <style>
-  a {
-    text-decoration: none;
-  }
-  a:hover {
-    color: red;
-  }
-  .tooltipS {
-    position: relative;
-    display: inline-block;
-    color: red;
-    font-weight: bold;
-  }
-  .tooltipS .tooltiptext {
-    visibility: hidden;
-    width: auto;
-    max-width: 300px;
-    background-color: black;
-    color: #fff;
-    text-align: center;
-    border-radius: 6px;
-    padding: 5px;
-    opacity: 0.8;
+a {
+  text-decoration: none;
+}
+a:hover {
+  color: red;
+}
+.tooltipS {
+  position: relative;
+  display: inline-block;
+  color: red;
+  font-weight: bold;
+}
+.tooltipS .tooltiptext {
+  visibility: hidden;
+  width: auto;
+  max-width: 300px;
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px;
+  opacity: 0.8;
 
-    /* Position the tooltip */
-    position: absolute;
-    z-index: 1;
-    top: -25px;
-    left: 105%;
-  }
+  /* Position the tooltip */
+  position: absolute;
+  z-index: 1;
+  top: -25px;
+  left: 105%;
+}
 
-  .tooltipS:hover .tooltiptext {
-    visibility: visible;
-  }
-  .tooltipS:focus .tooltiptext {
-    visibility: visible;
-  }
-  span {
-    white-space: pre-wrap;
-  }
-  .articleBoard {
-    border: 1px solid black;
-    border-radius: 0.2rem;
-    margin: 1rem;
-    padding: 0.5rem;
-  }
+.tooltipS:hover .tooltiptext {
+  visibility: visible;
+}
+.tooltipS:focus .tooltiptext {
+  visibility: visible;
+}
+span {
+  white-space: pre-wrap;
+}
+.articleBoard {
+  border: 1px solid black;
+  border-radius: 0.2rem;
+  margin: 1rem;
+  padding: 0.5rem;
+}
 </style>
 
 <template>
@@ -53,14 +53,17 @@
         <div class="col-sm-3">
           <label>Choose Pack</label>
         </div>
-        <app-combobox classValue="col-sm-6" :mainData="packComboboxData" @comboboxChange="selectedPack = $event" />
+        <app-combobox
+          classValue="col-sm-6"
+          :mainData="packComboboxData"
+          @comboboxChange="selectedPack = $event"
+        />
 
         <div class="col-sm-3">
           <button
             type="button"
             class="btn btn-primary float-right"
-            data-toggle="modal"
-            data-target=".bd-example-modal-sm"
+            @click="addWordButtonClick()"
           >
             Add Word
           </button>
@@ -109,12 +112,22 @@
             rows="15"
             style="margin-top: 0.5rem"
           ></textarea>
-          <small class="text-danger" v-if="!$v.articleData.article.required">Article is required !</small>
+          <small class="text-danger" v-if="!$v.articleData.article.required"
+            >Article is required !</small
+          >
         </div>
-        <button @click="readButtonClick" class="btn btn-success btn-lg" :disabled="!$v.selectedPack.required">
+        <button
+          @click="readButtonClick"
+          class="btn btn-success btn-lg"
+          :disabled="!$v.selectedPack.required"
+        >
           READ
         </button>
-        <button @click="clickNewArticleButton" class="btn btn-primary btn-lg" style="margin-left: 0.5rem">
+        <button
+          @click="clickNewArticleButton"
+          class="btn btn-primary btn-lg"
+          style="margin-left: 0.5rem"
+        >
           CHANGE ARTICLE
         </button>
         <button
@@ -126,53 +139,89 @@
           SAVE ARTICLE
         </button>
 
-        <p class="articleBoard col-xs-12" v-html="readArticle" v-if="!openTextArea"></p>
+        <p
+          class="articleBoard col-xs-12"
+          v-html="readArticle"
+          v-if="!openTextArea"
+        ></p>
       </div>
     </div>
-    <!-- Modal -->
 
+    <!-- Modal -->
     <div
       class="modal fade bd-example-modal-sm"
       tabindex="-1"
       role="dialog"
       aria-labelledby="mySmallModalLabel"
       aria-hidden="true"
-      style="margin: 5rem"
+      v-bind:class="{
+        show: modalDisplay,
+      }"
+      v-bind:style="{
+        display: modalDisplayValue,
+      }"
     >
-      <div class="modal-dialog">
+      <div class="modal-dialog" id="addWordModal">
         <div class="modal-content">
-          <div style="margin: 5rem">
-            <app-input
-              @input="$v.wordData.mainWord.$touch()"
-              :inputClass="{
-                'form-control': true,
-                'is-invalid': $v.wordData.mainWord.$error,
-              }"
-              inputId="mainWord"
-              label="Main Word"
-              divClass="col-md-12 col-xs-12"
-              v-model="wordData.mainWord"
-              :dataValue="wordData.mainWord"
-            ></app-input>
-            <app-input
-              @input="$v.wordData.secondaryWord.$touch()"
-              :inputClass="{
-                'form-control': true,
-                'is-invalid': $v.wordData.secondaryWord.$error,
-              }"
-              inputId="secondaryWord"
-              label="Secondary Word"
-              divClass="col-md-12 col-xs-12"
-              v-model="wordData.secondaryWord"
-              :dataValue="wordData.secondaryWord"
-            ></app-input>
-            <button
-              class="btn btn-success col-md-3 center"
-              @click="insertOrUpdateWord()"
-              :disabled="$v.wordData.$invalid"
-            >
-              ADD
-            </button>
+          <div style="margin: 2rem">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLongTitle">Save Word</h5>
+              <button
+                type="button"
+                @click="modalClick()"
+                style="
+                  border: 0;
+                  padding: 3px;
+                  background-color: #fff;
+                  cursor: pointer;
+                "
+              >
+                <span>X</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <app-input
+                @input="$v.wordData.mainWord.$touch()"
+                :inputClass="{
+                  'form-control': true,
+                  'is-invalid': $v.wordData.mainWord.$error,
+                }"
+                inputId="mainWord"
+                label="Main Word"
+                divClass="col-md-12 col-xs-12"
+                v-model="wordData.mainWord"
+                :dataValue="wordData.mainWord"
+                :attention1="$v.wordData.mainWord.required"
+                attention1Text="Main word is required !"
+              ></app-input>
+              <app-input
+                @input="$v.wordData.secondaryWord.$touch()"
+                :inputClass="{
+                  'form-control': true,
+                  'is-invalid': $v.wordData.secondaryWord.$error,
+                }"
+                inputId="secondaryWord"
+                label="Secondary Word"
+                divClass="col-md-12 col-xs-12"
+                v-model="wordData.secondaryWord"
+                :dataValue="wordData.secondaryWord"
+                :attention1="$v.wordData.secondaryWord.required"
+                attention1Text="Secondary word is required !"
+              ></app-input>
+            </div>
+            <div class="modal-footer">
+              <button
+                class="btn btn-success center"
+                @click="insertOrUpdateWord()"
+                :disabled="$v.wordData.$invalid"
+              >
+                <i class="far fa-save" style="margin-right: 3px"></i> SAVE
+              </button>
+              <button class="btn btn-danger center" @click="modalClick()">
+                <i class="far fa-window-close" style="margin-right: 3px"></i>
+                Close
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -185,184 +234,202 @@
 </template>
 
 <script>
-  import axios from "axios";
-  import { required } from "vuelidate/lib/validators";
-  import Combobox from "../components/Combobox.vue";
-  import Input from "../components/Input.vue";
-  import ArticleList from "../components/ArticleList.vue";
+import axios from "axios";
+import { required } from "vuelidate/lib/validators";
+import Combobox from "../components/Combobox.vue";
+import Input from "../components/Input.vue";
+import ArticleList from "../components/ArticleList.vue";
 
-  export default {
-    data() {
-      return {
-        selectData: "",
-        selectedPack: "",
-        packComboboxData: [],
-        words: [],
-        article: "",
-        readArticle: "",
-        openTextArea: true,
-        clickedReadButton: false,
-        wordData: {
-          mainWord: "",
-          secondaryWord: "",
-        },
-        articleData: {
-          title: "",
-          description: "",
-          article: "",
-        },
-      };
-    },
-    validations: {
-      articleData: {
-        title: { required },
-        description: { required },
-        article: { required },
-      },
+export default {
+  data() {
+    return {
+      modalDisplayValue: "none",
+      modalDisplay: false,
+      selectData: "",
+      selectedPack: "",
+      packComboboxData: [],
+      words: [],
+      article: "",
+      readArticle: "",
+      openTextArea: true,
+      clickedReadButton: false,
       wordData: {
-        mainWord: { required },
-        secondaryWord: { required },
+        mainWord: "",
+        secondaryWord: "",
       },
-      selectedPack: {
-        required,
+      articleData: {
+        title: "",
+        description: "",
+        article: "",
       },
+    };
+  },
+  validations: {
+    articleData: {
+      title: { required },
+      description: { required },
+      article: { required },
     },
-    name: "Edit",
-    components: {
-      "app-combobox": Combobox,
-      "app-input": Input,
-      ArticleList,
+    wordData: {
+      mainWord: { required },
+      secondaryWord: { required },
     },
-    methods: {
-      async selectWord() {
-        this.selectData = window.getSelection().toString();
-        if (this.selectData == "") {
-          return;
-        }
-        this.wordData = {};
-        this.wordData.mainWord = this.selectData;
-
-        await axios
-          .get("/pack/" + this.selectedPack + "/word/" + this.selectData)
-          .then((response) => {
-            console.log(response.data.data[0]);
-            if (response.data.data[0] != undefined) {
-              this.wordData = response.data.data[0];
-            }
-          })
-          .catch((e) => console.log(e));
-      },
-      readButtonClick: async function() {
-        if (this.selectedPack == "") {
-          alert("Please select pack to read");
-          return;
-        }
-        if (this.clickedReadButton) return;
-
-        this.clickedReadButton = true;
-        this.openTextArea = false;
-
-        await axios
-          .get("/pack/" + this.selectedPack + "/words")
-          .then((response) => {
-            this.words = response.data.data[0].words;
-
-            this.words.sort(function(a, b) {
-              return b.mainWord.length - a.mainWord.length;
-            });
-            console.log(this.words);
-            this.readArticle = this.articleData.article;
-            this.words.forEach((element) => {
-              var regx = new RegExp("(\\b" + element.mainWord + "\\b)(?!^>)(?![^<>]*<*>)", "gi");
-              console.log(regx);
-              this.readArticle = this.readArticle.replace(
-                regx,
-                `<a href="#" class="tooltipS">${element.mainWord}<span class="tooltiptext">${element.secondaryWord}</span></a>`
-              );
-            });
-          })
-          .catch((e) => console.log(e));
-      },
-      clickNewArticleButton() {
-        this.clickedReadButton = false;
-        this.openTextArea = true;
-      },
-      readToken() {
-        this.tokenNow = this.$store.state.token;
-      },
-      insertOrUpdateWord: async function() {
-        //işlem uygulanıcak data varsa update yoksa add
-        if (this.wordData._id === undefined) {
-          await axios
-            .post("/pack/" + this.selectedPack + "/word", { ...this.wordData })
-            .then((response) => {
-              console.log(response);
-              alert("Word added");
-            })
-            .catch((e) => console.log(e));
-        } else {
-          await axios
-            .put("/pack/" + this.selectedPack + "/word", { ...this.wordData })
-            .then((response) => {
-              console.log(response);
-              alert("Word updated");
-            })
-            .catch((e) => console.log(e));
-        }
-        this.wordData = {};
-      },
-      saveArticle: async function() {
-        if (this.articleData.title === "") {
-          return;
-        }
-        var articleModel = {};
-        await axios
-          .get("/article/title/" + this.articleData.title)
-          .then((response) => {
-            if (response.data.data != []) {
-              articleModel = response.data.data[0];
-            }
-          })
-          .catch((e) => console.log(e));
-        console.log(articleModel);
-        if (articleModel === undefined) {
-          await axios
-            .post("/article/add/", { ...this.articleData })
-            .then((response) => {
-              console.log(response);
-              alert("Article added");
-            })
-            .catch((e) => console.log(e));
-        } else {
-          articleModel = { ...articleModel, ...this.articleData };
-          await axios
-            .put("/article/add/", { ...articleModel })
-            .then((response) => {
-              console.log(response);
-              alert("Article saved");
-            })
-            .catch((e) => console.log(e));
-        }
-      },
+    selectedPack: {
+      required,
     },
-    async created() {
-      axios.defaults.baseURL = process.env.VUE_APP_BASE_PATH;
-      axios.defaults.headers.common["Authorization"] = `Bearer: ${this.$store.state.token}`;
+  },
+  name: "Edit",
+  components: {
+    "app-combobox": Combobox,
+    "app-input": Input,
+    ArticleList,
+  },
+  methods: {
+    async selectWord() {
+      this.selectData = window.getSelection().toString();
+      if (this.selectData == "") {
+        return;
+      }
+      this.wordData = {};
+      this.wordData.mainWord = this.selectData;
+
       await axios
-        .get("/pack/forCbx")
+        .get("/pack/" + this.selectedPack + "/word/" + this.selectData)
         .then((response) => {
-          this.packComboboxData = response.data.data;
+          console.log(response.data.data[0]);
+          if (response.data.data[0] != undefined) {
+            this.wordData = response.data.data[0];
+          }
         })
-        .catch((e) => {
-          console.log(e.status, e.message);
-          this.$store.dispatch("clearToken");
-        });
+        .catch((e) => console.log(e));
     },
-    watch: {
-      articleData() {
-        this.clickedReadButton = false;
-        this.openTextArea = true;
-      },
+    readButtonClick: async function () {
+      if (this.selectedPack == "") {
+        alert("Please select pack to read");
+        return;
+      }
+      if (this.clickedReadButton) return;
+
+      this.clickedReadButton = true;
+      this.openTextArea = false;
+
+      await axios
+        .get("/pack/" + this.selectedPack + "/words")
+        .then((response) => {
+          this.words = response.data.data[0].words;
+
+          this.words.sort(function (a, b) {
+            return b.mainWord.length - a.mainWord.length;
+          });
+          console.log(this.words);
+          this.readArticle = this.articleData.article;
+          this.words.forEach((element) => {
+            var regx = new RegExp(
+              "(\\b" + element.mainWord + "\\b)(?!^>)(?![^<>]*<*>)",
+              "gi"
+            );
+            console.log(regx);
+            this.readArticle = this.readArticle.replace(
+              regx,
+              `<a href="#" class="tooltipS">${element.mainWord}<span class="tooltiptext">${element.secondaryWord}</span></a>`
+            );
+          });
+        })
+        .catch((e) => console.log(e));
     },
-  };
+    clickNewArticleButton() {
+      this.clickedReadButton = false;
+      this.openTextArea = true;
+    },
+    readToken() {
+      this.tokenNow = this.$store.state.token;
+    },
+    insertOrUpdateWord: async function () {
+      //işlem uygulanıcak data varsa update yoksa add
+      if (this.wordData._id === undefined) {
+        await axios
+          .post("/pack/" + this.selectedPack + "/word", { ...this.wordData })
+          .then((response) => {
+            console.log(response);
+            alert("Word added");
+          })
+          .catch((e) => console.log(e));
+      } else {
+        await axios
+          .put("/pack/" + this.selectedPack + "/word", { ...this.wordData })
+          .then((response) => {
+            console.log(response);
+            alert("Word updated");
+          })
+          .catch((e) => console.log(e));
+      }
+      this.wordData = {};
+    },
+    saveArticle: async function () {
+      if (this.articleData.title === "") {
+        return;
+      }
+      var articleModel = {};
+      await axios
+        .get("/article/title/" + this.articleData.title)
+        .then((response) => {
+          if (response.data.data != []) {
+            articleModel = response.data.data[0];
+          }
+        })
+        .catch((e) => console.log(e));
+      console.log(articleModel);
+      if (articleModel === undefined) {
+        await axios
+          .post("/article/add/", { ...this.articleData })
+          .then((response) => {
+            console.log(response);
+            alert("Article added");
+          })
+          .catch((e) => console.log(e));
+      } else {
+        articleModel = { ...articleModel, ...this.articleData };
+        await axios
+          .put("/article/add/", { ...articleModel })
+          .then((response) => {
+            console.log(response);
+            alert("Article saved");
+          })
+          .catch((e) => console.log(e));
+      }
+    },
+    modalClick: function () {
+      this.modalDisplay = !this.modalDisplay;
+      if (this.modalDisplay) {
+        this.modalDisplayValue = "block";
+      } else {
+        this.modalDisplayValue = "none";
+      }
+    },
+    async addWordButtonClick() {
+      this.modalClick();
+    },
+  },
+  async created() {
+    axios.defaults.baseURL = process.env.VUE_APP_BASE_PATH;
+    axios.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer: ${this.$store.state.token}`;
+    await axios
+      .get("/pack/forCbx")
+      .then((response) => {
+        this.packComboboxData = response.data.data;
+      })
+      .catch((e) => {
+        console.log(e.status, e.message);
+        this.$store.dispatch("clearToken");
+      });
+  },
+  watch: {
+    articleData() {
+      this.clickedReadButton = false;
+      this.openTextArea = true;
+    },
+  },
+};
 </script>
