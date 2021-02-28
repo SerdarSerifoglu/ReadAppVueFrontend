a7d996a48f89546b0bcb22e985fa3eaf4e6d1a0f
SerdarSerifoglu <serdarserifoglu@gmail.com>
2021-02-28 16:05:13 +0300

    store'da localStorage'da tuttuğumuz token alanı atıl kalmıştı temizlendi. Login işlemi store'dan kaldırıldı.

--- src/store/index.js
+++ src/store/index.js
@@ -3,18 +3,11 @@ import Vuex from "vuex";
 import axios from "axios";
 import router from "../router/index.js";
 import createPersistedState from "vuex-persistedstate";
-
 Vue.use(Vuex);
-axios.defaults.baseURL = process.env.VUE_APP_BASE_PATH;
-if (localStorage.getItem("token")) {
-  axios.defaults.headers.common[
-    "Authorization"
-  ] = `Bearer: ${localStorage.getItem("token")}`;
-}
 export default new Vuex.Store({
   plugins: [createPersistedState()],
   state: {
-    token: localStorage.getItem("token") || "",
+    token: "",
     userSettings: {
       _id: "",
       color: "",
@@ -25,14 +18,12 @@ export default new Vuex.Store({
   mutations: {
     setToken(state, token) {
       state.token = token;
-      localStorage.setItem("token", token);
     },
     clearToken(state) {
-      state.token = "";
-      localStorage.setItem("token", "");
+      state;
       router.push("/login");
     },
-    setSelectedPack(state, data) {
+    setUserSetting(state, data) {
       state.userSettings.color = data.wordColor;
       state.userSettings.selectedPackId = data.selectedPackId;
       state.userSettings._id = data._id;
@@ -54,52 +45,21 @@ export default new Vuex.Store({
     closeLoading({ commit }) {
       commit("closeLoading");
     },
-    async initAuth({ commit }) {
-      let token = localStorage.getItem("token");
-      console.log("Token:" + token);
-      if (token != "") {
-        await axios
-          .get("/auth/tokentest")
-          .then(response => {
-            console.log(response.data);
-            if (response.data.success === false) {
-              commit("clearToken");
-              router.push("/login");
-            } else {
-              commit("setToken", token);
-            }
-          })
-          .catch(e => {
-            router.push("/login");
-            console.log(e);
-          });
-      } else {
-        commit("clearToken");
-        router.push("/login");
-      }
-    },
-    login({ commit }, authData) {
-      axios
-        .post("/auth/login", { ...authData })
-        .then(response => {
-          commit("setToken", response.data.access_token);
-          router.push("/reader");
-        })
-        .catch(e => console.log(e));
-    },
     logout({ commit }) {
       commit("clearToken");
-      router.push({ name: "Login" });
-      // router.push("/login");
     },
     clearToken({ commit }) {
       commit("clearToken");
+    },
+    setToken({ commit, payload }) {
+      console.log(payload);
+      commit("setToken", payload);
     },
     setUserSettings({ commit }) {
       axios
         .get("/userSetting/getUserSettings")
         .then(response => {
-          commit("setSelectedPack", response.data.data);
+          commit("setUserSetting", response.data.data);
         })
         .catch(e => console.log(e));
     },
@@ -114,6 +74,9 @@ export default new Vuex.Store({
     },
     getLoadingStatus(state) {
       return state.isLoading;
+    },
+    getStateToken(state) {
+      return state.token;
     },
   },
   modules: {},
