89fa43cb314c56cc7850b4113c5c33b59ec7cc37
SerdarSerifoglu <serdarserifoglu@gmail.com>
2021-03-04 23:04:43 +0300

    as

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express();
 
-app.use("/", serveStatic(path.join(_dirname, "/dist")));
+app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
 const port = process.env.PORT || 8080;
 app.listen(port);
