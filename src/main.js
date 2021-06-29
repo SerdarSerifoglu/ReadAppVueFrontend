2112158d2d3c66f7f56df6c799086231e584eb70
SerdarSerifoglu <serdarserifoglu@gmail.com>
2021-06-29 15:22:01 +0300

    otomatik login işlemi gerçekleştirildi

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from "./store";
 import Vuelidate from "vuelidate";
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
+import axios from "axios";
 
 Vue.use(Buefy);
 
@@ -14,5 +15,21 @@ Vue.use(Vuelidate);
 new Vue({
   router,
   store,
+  created() {
+    const userString = localStorage.getItem("user");
+    if (userString) {
+      const userData = JSON.parse(userString);
+      this.$store.commit("SET_USER_DATA", userData);
+    }
+    axios.interceptors.response.use(
+      response => response,
+      error => {
+        if (error.response.status === 401) {
+          this.$store.dispatch("logout");
+        }
+        return Promise.reject(error);
+      }
+    );
+  },
   render: h => h(App),
 }).$mount("#app");
